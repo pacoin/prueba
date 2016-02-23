f0127c129cfe27d70d5e13a3b6a3dc809af63428
Francisco Leija <franciscoleija@macbook-pro-de-francisco.local>
2016-02-23 01:25:28 -0600

    Initial Commit

---
+++ PruebaGit_2Tests/PruebaGit_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PruebaGit_2Tests.swift
+//  PruebaGit_2Tests
+//
+//  Created by Francisco  Leija on 23/02/16.
+//  Copyright © 2016 Francisco  Leija. All rights reserved.
+//
+
+import XCTest
+@testable import PruebaGit_2
+
+class PruebaGit_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
